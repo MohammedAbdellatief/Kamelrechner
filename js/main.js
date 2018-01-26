0add3755fc72f039bce9d8c60c003ac004cd2f40
Mohammed Abdellatief <mabdellatief@gmail.com>
2018-01-26 19:42:20 +0200

    creating animation for the camel

--- js/main.js
+++ js/main.js
@@ -2,26 +2,31 @@
 $(function() {
 
     // selectors
-    var ageOutput = document.querySelectorAll('.age-output')[0];
-    var heightOutput = document.querySelectorAll('.height-output')[0];
+    var form = $('.form-container, .title');
+    var result = $('.result-container');
+    var slider = $('input[type=range]');
+    var ageOutput = $('.age-output');
+    var heightOutput = $('.height-output');
     var hairColor = 'black'; //default value
     var eyeColor = 'blue'; //default value
     var muscles = 'normal'; //default value
     var food = 'vegetarian'; //default value
 
 
-    // update range slider output on slide
+    // update range slider output when slide changes
     $(document).on('input', '#ageInput', function(e) {
-        ageOutput.innerHTML = e.currentTarget.value;
+        ageOutput.html(e.currentTarget.value);
     });
     $(document).on('input', '#heightInput', function(e) {
-        heightOutput.innerHTML = e.currentTarget.value;
+        heightOutput.html(e.currentTarget.value);
     });
 
     //initiate range slider
-    $('input[type=range]').rangeslider({
+    slider.rangeslider({
         polyfill: false
     });
+
+
 
     //handle radio button hair color change
     $('input[type=radio][name=hairColor]').change(function (e) {
@@ -55,15 +60,14 @@ $(function() {
 
 
     //when slider changes, hide "Slide" message
-    $("input").on("change", function() {
-        $("small").fadeOut("slow");
+    slider.on("change", function() {
+        $(".slideRight").fadeOut("slow");
     });
 
 
     //on form submit calculate camels
     $('#camel-calculator-form').on('submit', function (e) {
         e.preventDefault();
-
         var age = $('.age-output').html(),
             height = $('.height-output').html();
 
@@ -76,8 +80,35 @@ $(function() {
             foodHabit: food
         });
 
-        $('#number-of-camels').html(numberOfCamels)
-    })
+        showResult(numberOfCamels);
+
+    });
+
+
+    //show result and hide form
+    var showResult = function (num) {
+
+        form.fadeOut('fast');
+        result.fadeIn('slow');
+
+        $('#number-of-camels').each(function () {
+            var $this = $(this);
+            $({ Counter: 0 }).animate({ Counter: num }, {
+                duration: 1500,
+                easing: 'swing',
+                step: function () {
+                    $this.text(Math.ceil(this.Counter));
+                }
+            });
+        });
+    };
+
+    // Reset and calculate again
+    $('#resetButton').on('click', function (e) {
+        result.fadeOut('fast');
+        form.fadeIn('slow');
+    });
+
 
 
 
